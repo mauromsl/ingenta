f779d95f2061585694a3a603e3d46da94e1155ad
Mauro MSL <mauro.msl@outlook.com>
2020-12-08 14:29:01 +0000

    Adds index view

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from plugins.imports import views
+from plugins.ingenta import views
 
 urlpatterns = [
     url(r'^$', views.index, name='ingenta_index'),
--- views.py
+++ views.py
@@ -4,6 +4,8 @@ from django.core.urlresolvers import reverse
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
 
+from identifiers import models as id_models
+
 
 
 @staff_member_required
@@ -13,9 +15,18 @@ def index(request):
     :param request: HttpRequest
     :return: HttpResponse
     """
+    ingenta_ids = id_models.Identifier.objects.filter(
+        id_type="ingenta_id",
+    ).select_related(
+        "article",
+    )
+    if request.journal:
+        ingenta_ids.filter(article__journal=request.journal)
+    ingenta_ids.order_by("-article__date_published")
 
     template = "ingenta/index.html"
     context = {
+        "ingenta_ids": ingenta_ids,
     }
 
     return render(request, template, context)
